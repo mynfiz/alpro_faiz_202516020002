da6973f26c87c6ce1d9080d0be02956f0f7a3e8d
mynfiz <ahmadfazot@gmail.com>
2026-03-04 20:11:54 +0700

    Add files via upload

---
+++ pertemuan-2/2-luas-persegi-panjang.py
@@ -0,0 +1,5 @@
+print ("---> MENGHITUNG LUAS PERSEGI PANJANG <---")
+p = float(input("Masukan nilai panjang : "))
+l = float(input("Masukan nilai luas : "))
+L = p * l
+print("Luas persegi panjang adalah : ", L)
