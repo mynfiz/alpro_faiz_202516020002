5448e5ea2520d7452edd991b23891183ad9b7b79
mynfiz <ahmadfazot@gmail.com>
2026-03-04 20:42:43 +0700

    Delete pertemuan-1 directory

--- pertemuan-1/2-keliling-persegi.py
+++
@@ -1,9 +0,0 @@
-#mulai
-print("---> HITUNG KELILING PERSEGI <---\n")
-#judul
-s = int(input("Masukan nilai sisi persegi: "))
-#input bertujuan untuk memasukan atau menerima nilai dari pengguna
-K = 4 * s
-#rumus keliling persegi
-print("Keliling persegi adalah : ", K)
-#selesai
