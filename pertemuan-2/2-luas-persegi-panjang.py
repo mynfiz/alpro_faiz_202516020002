2251f0dcb7e38c58c80e62894bf213f61096a5e6
mynfiz <ahmadfazot@gmail.com>
2026-03-04 20:42:53 +0700

    Delete pertemuan-2 directory

--- pertemuan-2/2-luas-persegi-panjang.py
+++
@@ -1,5 +0,0 @@
-print ("---> MENGHITUNG LUAS PERSEGI PANJANG <---")
-p = float(input("Masukan nilai panjang : "))
-l = float(input("Masukan nilai luas : "))
-L = p * l
-print("Luas persegi panjang adalah : ", L)
